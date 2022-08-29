84a2dd102d5bcf74fac078f682e8ff670b0171d3
Miroslav Peshkov <99148327+MiroslavPeshkov@users.noreply.github.com>
2022-08-29 18:36:44 +0300

    Delete create_csv.py

--- create_csv.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open(f'PARSING.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    datawriter = csv.writer(csvfile, delimiter=',',
-                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    datawriter.writerow(
-        ['Дата'] + ['Конкурент'] + ['Ссылка'] + ['Артикул'] + ['Тип'] + ['CCT'] + ['CRI'] + ['Uнoм,В'] + ['Бренд'] + [
-            'Цена'] + ['Остаток'] + ['Серия'] + ['Iбин.'] + ['Iмакс'] + ['Фмин'] + ['Фтип'] + ['Фмакс'] + ['Uмин'] + [
-            'Uтип'] + ['Uмакс'] + ['Datasheet'])
